9904f8c9b79cc741bcacf3216c4079d484b0cbed
Yupagom <158861910+Yupagom@users.noreply.github.com>
2024-03-12 21:01:01 +0000

    disenio movil

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 // App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Container, AppBar, Toolbar, Typography, Box, Link, List, ListItem, ListItemText } from '@mui/material';
+import { Container, AppBar, Toolbar, Typography, Link, List, ListItem, Drawer, ListItemText, useMediaQuery, IconButton } from '@mui/material';
 import Home from './components/Home/Home';
 import AcercaDe from './components/AcercaDe/AcerdaDe';
 import Servicios from './components/Servicios/Servicios';
 import Footer from './components/Footer/Footer';
 import Productos from './components/Productos/Productos';
+import MenuIcon from '@mui/icons-material/Menu';
+
 import './App.css';
 
-
 const App = () => {
+
+  const isMobile = useMediaQuery('(max-width:600px)');
+
+  const [drawerOpen, setDrawerOpen] = React.useState(false);
+
+  const toggleDrawer = (open) => (event) => {
+    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    setDrawerOpen(open);
+  };
+
   return (
     <Router>
       <AppBar position="static">
@@ -20,7 +33,16 @@ const App = () => {
               AG Accesorios Gómez
             </Link>
           </Typography>
-          <List component="nav" aria-labelledby="main navigation" className='menu-nav'>
+          <IconButton
+            edge="end"
+            color="inherit"
+            aria-label="menu"
+            onClick={toggleDrawer(true)}
+            sx={{ display: { xs: 'block', sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <List component="nav" aria-labelledby="main navigation" className='menu-nav' sx={{ display: { xs: 'none !important', sm: 'block !important' } }}>
             <ListItem>
               <Link href="/" color="inherit" underline="none">
                 Inicio
@@ -43,6 +65,30 @@ const App = () => {
             </ListItem>
           </List>
         </Toolbar>
+        <Drawer anchor="right" open={drawerOpen} onClose={toggleDrawer(false)}>
+          <List>
+            <ListItem button>
+              <Link href="/" color="inherit" underline="none">
+                Inicio
+              </Link>
+            </ListItem>
+            <ListItem button>
+              <Link href="/about" color="inherit" underline="none">
+                Acerca de
+              </Link>
+            </ListItem>
+            <ListItem button>
+              <Link href="/servicios" color="inherit" underline="none">
+                Servicios
+              </Link>
+            </ListItem>
+            <ListItem button>
+              <Link href="/productos" color="inherit" underline="none">
+                Productos
+              </Link>
+            </ListItem>
+          </List>
+        </Drawer>
       </AppBar>
       <Container>
         <Routes>
